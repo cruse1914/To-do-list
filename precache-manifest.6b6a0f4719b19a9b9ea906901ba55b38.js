7e0725548bcb034d6414855b42fe71210fcbba1a
cruse1914 <contact.dm07@gmail.com>
2021-03-17 01:29:26 +0530

    Updates

---
+++ precache-manifest.6b6a0f4719b19a9b9ea906901ba55b38.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "749a0bd2a922243e4fff6de86a232009",
+    "url": "/To-do-list/index.html"
+  },
+  {
+    "revision": "e5f2aec77137107e26d1",
+    "url": "/To-do-list/static/js/2.c475bd67.chunk.js"
+  },
+  {
+    "revision": "f449079cbd6078f7608c",
+    "url": "/To-do-list/static/js/main.ccd138cb.chunk.js"
+  },
+  {
+    "revision": "eef5bd35e101595273e0",
+    "url": "/To-do-list/static/js/runtime-main.5ebe4cab.js"
+  }
+]);
